9d5899d6a1750af9fad4d7c6f1377db90e4c4007
unknown <jquijano3842@gmail.com>
2021-11-30 16:08:34 -0700

    nuevo

--- header.php
+++ header.php
@@ -15,6 +15,9 @@
 			<nav>
 				<ul>
 					<li><a href="index.php">Inicio</a></li>
+					<li><a href="productos.php">Productos</a></li>
+					<li><a href="cuidados.php">Cuidados</a></li>
+					<li><a href="cortes.php">Cortes</a></li>
 					<li><a href="bd/cita.php">Citas</a></li>
 				</ul>
 			</nav>
